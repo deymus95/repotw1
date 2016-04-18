ccae1e1974b1bc85ddbcae9ff2732556c158ebd6
deymus <deymus95@gmail.com>
2016-04-18 18:56:11 -0300

    Clase Cuadrado

---
+++ Cuadrado/src/ar/edu/tw1tm/unlam/www/Cuadrado.java
@@ -0,0 +1,11 @@
+package ar.edu.tw1tm.unlam.www;
+
+public class Cuadrado {
+	
+	private Integer perimetro;
+	
+	public Integer resultadoPerimetro(Integer lado){
+		return perimetro = lado * 4;
+	}
+
+}
---
+++ Cuadrado/test/ar/edu/tw1tm/unlam/www/CuadradoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.tw1tm.unlam.www;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CuadradoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
